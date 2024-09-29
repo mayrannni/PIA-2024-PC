0d52972fb47d60c0a17f9128e3cb095c96cce770
Edgar-A23 <edgar.rodriguezp@uanl.edu.mx>
2024-09-29 10:43:26 -0600

    Update scanning.sh

--- BASH/scanning.sh
+++ BASH/scanning.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Functions
+# Functions
 ports_scan() {
-    read -p ">>Enter the range of ports youu want to scan (Example 1-100 or only one like 22): " ports
-    read -p ">>Enter the IP to scan: " IP #The IP has the following format ***.***.***.*** 0-255
-    read -p ">>Enter the name of the file where yout results will be saved (just the file name not the path): " file
-    #check that the file ends in .txt if not added
+    read -p ">>Enter the range of ports you want to scan (Example 1-100 or only one like 22): " ports
+    read -p ">>Enter the IP to scan: " IP # The IP has the following format ***.***.***.*** 0-255
+    read -p ">>Enter the name of the file where your results will be saved (just the file name, not the path): " file
+    # Check that the file ends in .txt if not added
     if [[ $file =~ \.txt*$ ]]; then
         file=$file
     else
@@ -13,49 +13,59 @@ ports_scan() {
     fi
     path="$PWD/$file"
     errors="$PWD/errorsPortScan.log"
-    #Create a new file
+    # Create a new file
     touch $path
-    #Extract open ports from a given IP
+    # Extract open ports from a given IP
     nmap -p $ports $IP > $path 2> $errors
+    # Error manage
     if [[ -s $errors ]];then
-        echo "We have an errors wich are saved in: $errors"
+        echo "We have errors which are saved in: $errors"
     else
+        # If we have no error the error file will be removed
         rm $errors
         echo "The port scan has completed and the file has been saved to: $path"
     fi
 }
 
 vulnerability_scan() {
-    read -p ">>Enter the IP to scan: " IP #The IP has the following format ***.***.***.*** 0-255
-    read -p ">>Enter the name of the file where yout results will be saved (please include .txt termination): " file
-    #check that the file ends in .txt if not added
+    read -p ">>Enter the IP to scan: " IP # The IP has the following format ***.***.***.*** 0-255
+    read -p ">>Enter the name of the file where your results will be saved (please include .txt termination): " file
+    # Check that the file ends in .txt if not added
     if [[ $file =~ \.txt*$ ]]; then
         file=$file
     else
         file="$file.txt"
     fi
+    # Create the path for the port open Scan, vulnerabilities scan, and errors
     path="$PWD/$file"
     pathScan="$PWD/test.txt"
     errorsV="$PWD/errorsVuln.log"
-    #Create a new file
+    # Create a new file for the open ports scan
     touch $pathScan
-    #Chek open ports and extract vulnerabilities
-    nmap -p $ports $IP | grep 'open' > $pathScan 2> $errorsV
+    # Chek open ports and extract vulnerabilities
+    nmap $IP | grep 'open' > $pathScan 2> $errorsV
     if [[ -s $errorsV ]];then
-        echo "We have an errors wich are saved in: $errors"
+        echo "We have errors which are saved in: $errors"
     else
+        # If the last command executes correctly the error.log file will be deleted 
         rm $errorsV
+        # Reviewing the output from the open ports scan to check if there is at least one open port
         if [[ -s $pathScan ]];then
+            # Create the vulnerabilites 
             touch $path
+            # If we have at least one open port we do the nmap vulnerabilities scan
             nmap -sV --script=vuln $IP > $path 2> $errorsV
-            if [-s $errorsV];then
-                echo "We have an errors wich are saved in: $errorsV"
+            if [ -s $errorsV ];then
+                # The error manage
+                echo "We have errors which are saved in: $errorsV"
             else
+                # If there is no error in the vulnerabilities scan we remove the test ports open scan file and error file
                 rm $errorsV
                 rm $pathScan
-                echo "The port scan has completed and the file has been saved to: $path"
+                echo "The port scan has completed and the file has been saved in: $path"
             fi
         else
+            # If the file of the open ports scan is empty (that means there are no open ports) the vulnerabilities scan will not realesed
             echo "The scan did not run because there are no ports open"
         fi
     fi
