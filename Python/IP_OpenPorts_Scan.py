957e32b74902111442f4a142458e026af5ba9c11
Edgar-A23 <edgar.rodriguezp@uanl.edu.mx>
2024-10-16 23:01:42 -0600

    Update IP_OpenPorts_Scan.py

--- Python/IP_OpenPorts_Scan.py
+++ Python/IP_OpenPorts_Scan.py
@@ -10,8 +10,8 @@ logging.basicConfig(filename='APIRequest_Errors.log',
                     level=logging.ERROR)
 
 # Create an argument for the sripts run
-ips = argparse.ArgumentParser("IPs to make the scan of their ports")
-ips.add_argument("-ip", dest="IP", type=str, help="IP to analyze ", required=True)
+ips = argparse.ArgumentParser(description="IPs to make the scan of their ports")
+ips.add_argument("-ip", dest='IP',type=str, help="IP to analyze ", required=True)
 
 # The shodan api key to authenticate our search in the api
 api_key = 'qYsYnBh8c6vx820iWeJc9VwzFMcIUU5l'
@@ -24,8 +24,9 @@ except shodan.APIError as error:
 # Save the response in a txt file to make reading easier
 with open('API_IPResponse.txt', 'w') as file:
     try:
-        response = api.host(ip)
-        file.write(f"<< Open ports in IP : {ip} >>\n")
+        ip = ips.parse_args()
+        response = api.host(ip.IP)
+        file.write(f"<< Open ports in IP : {ip.IP} >>\n")
         for port in response['ports']:
             file.write(f"> {port} <\n")
         file.write("Scan complete, Have a nice day :D")
@@ -39,8 +40,12 @@ with open('API_IPResponse.txt', 'w') as file:
         logging.error(f"Error: {error}")
 
     else:
-        response = api.host(ip)
-        full_response_file_name = 'Response.txt'
+        response = api.host(ip.IP)
+        full_response_file_name = 'Full_API_Response.txt'
         with open(full_response_file_name, 'w') as file:
             data = json.dumps(response, indent=4)
             file.write(data)
+    
+    finally:
+        print("The full response and error.log files are created"\
+              " with the IP scan requested")
